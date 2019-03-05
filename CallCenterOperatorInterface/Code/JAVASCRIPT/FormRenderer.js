f48ad6588d6150375c2c54e76baf8378c4072dae
rspyxxx <48235277+rspyxxx@users.noreply.github.com>
2019-03-06 01:04:46 +0800

    Update FormRenderer.js

--- CallCenterOperatorInterface/Code/JAVASCRIPT/FormRenderer.js
+++ CallCenterOperatorInterface/Code/JAVASCRIPT/FormRenderer.js
@@ -36,7 +36,32 @@ function validateForm() {
 		respondent: respondent,
 		description: description
 		};
-	
+	<?php
+		// form renderer
+
+		include_once 'db_connect.php'; //need to add the path for this file
+
+		$name = $_POST['name'];
+		$contact = $_POST['mobileNum'];
+		$pc = $_POST['postalCode'];
+		$building = $_POST['building'];
+		$respondent = $_POST['respondent'];
+		$description = $_POST['description'];
+		$username = $_POST['username'];
+
+		if (empty($building)) {
+			$location = $pc;
+		}
+		else {
+			$location = $building.",".$pc;
+		}
+
+		$query = 
+		"INSERT INTO incidentTbl  (name, contact, location, assistanceType, description, ins_name,upd_name)
+		VALUES ($name, $contact, $location, $respondent, $description, $username, $username)"
+
+	?>
+
 	// Testing
 	console.log(incident);
 	return false;
