987c40b1cbe6908e48f241124c02503554dd7852
rspyxxx <48235277+rspyxxx@users.noreply.github.com>
2019-03-05 22:12:39 +0800

    Update consolidated_triggers.sql

--- CallCenterOperatorInterface/Code/Database/consolidated_triggers.sql
+++ CallCenterOperatorInterface/Code/Database/consolidated_triggers.sql
@@ -6,7 +6,7 @@
 -------------------------------------------------------------
 
 /* trigger for accountTbl */
--- ASSUMING username and sessionKey CANNOT be changed
+-- ASSUMING username and sessionKey will not be changed
 
 CREATE TRIGGER upd_account_time on [accountTbl] after update
 AS
@@ -25,3 +25,25 @@ BEGIN
 
 END
 GO
+
+
+
+/* trigger for incidentTbl */
+-- ASSUMPTION is that the only update that can be done on this table is updating of 'status' 
+CREATE TRIGGER upd_incident_time on [incidentTbl] after update
+AS 
+BEGIN 
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	
+	update incidentTbl
+	set upd_date = getdate()
+	where incidentTbl.recordID in
+	(select i.recordID from inserted i
+	inner join deleted d on i.recordId = d.recordId
+	where i.status <> d.status)
+END
+GO
+
+
