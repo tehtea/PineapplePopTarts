65c7019dbb6734494aea894263d8d5f6b9bdf827
rspyxxx <48235277+rspyxxx@users.noreply.github.com>
2019-03-05 21:09:23 +0800

    Create sample_account_ingestion.sql

---
+++ CallCenterOperatorInterface/Code/sample_account_ingestion.sql
@@ -0,0 +1,15 @@
+-------------------------------------------------------------
+-- CZ3003 GROUP SSP1 
+-- PineapplePoptarts
+-- SOFTWARE LAB 3 
+-- DATABASE SCHEMA: INGESTION OF SAMPLE CALL OPERATOR ACCOUNT
+-------------------------------------------------------------
+
+/* create some account for testing/demo */
+
+INSERT INTO accountTbl (username, password, sessionkey)
+VALUES ('ted','TED', 'CCO-1')
+INSERT INTO accountTbl (username, password, sessionkey)
+VALUES ('cheese','Pie', 'CCO-1')
+INSERT INTO accountTbl (username, password, sessionkey)
+VALUES ('shark','chicken', 'CCO-1')
