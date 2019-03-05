92265227942ad8b34161ccec42b5daa3d956298f
rspyxxx <48235277+rspyxxx@users.noreply.github.com>
2019-03-06 01:13:02 +0800

    Update AccountRenderer.js

--- CallCenterOperatorInterface/Code/JAVASCRIPT/AccountRenderer.js
+++ CallCenterOperatorInterface/Code/JAVASCRIPT/AccountRenderer.js
@@ -1,7 +1,3 @@
-<?php 
-	include_once 'db_connect.php'; //need to add the path for this file
-?>
-	
 loadUsername();
 
 function logout() {
@@ -20,6 +16,8 @@ function getUsername(sessionKey) {
 	<?php 
 		//account renderer 
 		//assuming only returning one username 
+		include_once 'db_connect.php'; //need to add the path for this file	
+
 		$var1 = $_POST['sessionKey'];
 		$query = "select username from accountTbl where sessionKey = $var1";
 		$results = mysqli_query($conn, $query);	
