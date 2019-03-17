2269bca49241e2d63ea716150e0a879972186836
tehtea <jingwei.chew@gmail.com>
2019-03-17 08:14:46 -0700

    fixed up the Map subsystem.

--- CrisisMap/Weather.js
+++ CrisisMap/Weather.js
@@ -1,5 +1,5 @@
 
-  function getData(){
+function getData(){
     //alert('ajax');
 
     var receivedData; // store your value here
@@ -9,24 +9,25 @@
         url: "https://api.data.gov.sg/v1/environment/2-hour-weather-forecast",
         async: false,
         success: function(data){
+            // console.log('got the data!');
             receivedData = data;
         }
     });
 
+    // console.log("returning from function");
     return receivedData;
-  };
-  
-  //get marker data from api (weather)
-  function getWeatherData(){
-    $.getScript("weatherapi.js");
+};
+
+//get marker data from api (weather)
+function getWeatherData(){
     var dataPoints;
     dataPoints = getData();
-    //console.log(dataPoints["area_metadata"]["0"]["label_location"]["latitude"]);
     for (let i=0; i<dataPoints["area_metadata"].length; i++){
-      inputs["weather"].push(dataPoints["area_metadata"][i]);
+        inputs["weather"].push(dataPoints["area_metadata"][i]); // inputs is a global variable for setting markers
     }
     data["weather"] = dataPoints;
-    console.log(data);
-  }
+    // console.log("weather data from Weather.js:");
+    // console.log(data);
+}
 
 // export {getData};
