c9a52b10c2dd21b573206198d79f95f850249aaa
Limzui <37795828+Limzui@users.noreply.github.com>
2019-03-22 23:57:50 +0800

    Fixed fetching incidents from database

--- StatusReportManager/keyIncidentFetcher.js
+++ StatusReportManager/keyIncidentFetcher.js
@@ -1,37 +1,19 @@
 module.exports = {
     fetchIncidents: function () {
-        // Run it every 5 seconds (parameter in miliseconds)
-        //        setInterval(() => {
-        let retrieval = new Promise(function (resolve, reject) {
+        return new Promise(function (resolve, reject) {
             var io = require("socket.io-client");
             var socket = io.connect('http://localhost:5000', { reconnect: true });
-            console.log('connect');
             // Connect to server
             socket.on('connect', function () {
                 // Request data from database
                 socket.emit('srRequest');
-                console.log('srRequest');
                 // Retrieve data
                 socket.on('srRequestDone', function (result) {
-                    console.log('resolve');
                     resolve(result);
                 });
             });
             // Disconnect
             socket.emit('disconnect');
         });
-        retrieval.then(function (result) {
-            // Insert your code here
-            //console.log(result);
-            console.log(result[0].RecordID);
-            return result;
-            /* Notes:
-            *	result[0] is newincident
-            *	result[1] is updateincident
-            *	result[2] is respondentRequest
-            */
-        });
-        //       }, 5000);
-
     },
 };
--- StatusReportManager/reportGenerator.js
+++ StatusReportManager/reportGenerator.js
@@ -1,94 +1,96 @@
 "use strict";
 var dateTimeGenerator = require('./dateTime.js');
 var keyIncidentFetcher = require('./KeyIncidentFetcher.js');
-//var keyIncidentFetcher = require('./OtherSubsystems.js');
 var apiDataFetcher = require('./apiDataFetcher.js');
 
 const docx = require("docx");
 const fs = require("fs");
 
 module.exports = {
-    generateReport: function() {
+    generateReport: function () {
         //fetch incidents
-        var incidents=null;
-        while (incidents == null){
-            incidents = keyIncidentFetcher.fetchIncidents();
-        }
-        console.log(result);
-        // Create empty document
-        console.log('Creating Empty Document...');
-        let doc = new docx.Document();
-        
-        // Add some content in the document
-        console.log('Adding Content...');
+        var incidents = keyIncidentFetcher.fetchIncidents();
+        incidents.then((result) => {
+            console.log('Incidents fetched!');
+            
+            // Create empty document
+            console.log('Creating Empty Document...');
+            let doc = new docx.Document();
 
-        //Define a break between paragraphs
-        let blankPara = new docx.Paragraph();
+            // Add some content in the document
+            console.log('Adding Content...');
 
-        //Define default heading para
-        let headingPara = new docx.Paragraph().heading1().left();
+            //Define a break between paragraphs
+            let blankPara = new docx.Paragraph();
 
-        //Add title
-        let titlePara = new docx.Paragraph().title().center();
-        titlePara.addRun(new docx.TextRun("Status Summary Report").bold());
-        doc.addParagraph(titlePara);
-        doc.addParagraph(blankPara);
-        let subtitlePara = new docx.Paragraph().left();
-        subtitlePara.addRun(new docx.TextRun("For the period ").size(24));
-        subtitlePara.addRun(new docx.TextRun(dateTimeGenerator.getDateTime30MinAgo()).size(24));
-        subtitlePara.addRun(new docx.TextRun(" till ").size(24));
-        subtitlePara.addRun(new docx.TextRun(dateTimeGenerator.getDateTimeNow()).size(24));
-        doc.addParagraph(subtitlePara);
-        doc.addParagraph(blankPara);
-        
+            //Define default heading para
+            let headingPara = new docx.Paragraph().heading1().left();
 
-        //Add New/Unresolved incidents TODO
-        headingPara.addRun(new docx.TextRun("Incident(s) Summary:").bold());
-        doc.addParagraph(headingPara);
+            //Add title
+            let titlePara = new docx.Paragraph().title().center();
+            titlePara.addRun(new docx.TextRun("Status Summary Report").bold());
+            doc.addParagraph(titlePara);
+            doc.addParagraph(blankPara);
+            let subtitlePara = new docx.Paragraph().left();
+            subtitlePara.addRun(new docx.TextRun("For the period ").size(24));
+            subtitlePara.addRun(new docx.TextRun(dateTimeGenerator.getDateTime30MinAgo()).size(24));
+            subtitlePara.addRun(new docx.TextRun(" till ").size(24));
+            subtitlePara.addRun(new docx.TextRun(dateTimeGenerator.getDateTimeNow()).size(24));
+            doc.addParagraph(subtitlePara);
+            doc.addParagraph(blankPara);
 
-        let incidentPara = new docx.Paragraph();
-        incidentPara.addRun(new docx.TextRun("New incidents in the past 30 minutes:\n"));
-        //incidentPara.addRun(new docx.TextRun(incidents));
-        
-        doc.addParagraph(blankPara);
 
-        //Add Key Indicators TODO
-        headingPara = new docx.Paragraph().heading1().left();
-        headingPara.addRun(new docx.TextRun("Key Indicator(s) Summary:").bold());
-        doc.addParagraph(headingPara);
+            //Add New/Unresolved incidents TODO
+            headingPara.addRun(new docx.TextRun("Incident(s) Summary:").bold());
+            doc.addParagraph(headingPara);
 
-        let indicatorPara = new docx.Paragraph();
-        indicatorPara.addRun(new docx.TextRun(""));
-        doc.addParagraph(indicatorPara);
-        doc.addParagraph(blankPara);
+            let incidentPara = new docx.Paragraph();
+            incidentPara.addRun(new docx.TextRun("New incidents in the past 30 minutes:\n"));
+            //incidentPara.addRun(new docx.TextRun());
+            doc.addParagraph(incidentPara);
 
-        //Add Trend(s) TODO
-        headingPara = new docx.Paragraph().heading1().left();
-        headingPara.addRun(new docx.TextRun("Trend(s) Summary:").bold());
-        doc.addParagraph(headingPara);
+            doc.addParagraph(blankPara);
 
-        let trendPara = new docx.Paragraph();
-        trendPara.addRun(new docx.TextRun(""));
-        doc.addParagraph(trendPara);
-        doc.addParagraph(blankPara);
+            //Add Key Indicators TODO
+            headingPara = new docx.Paragraph().heading1().left();
+            headingPara.addRun(new docx.TextRun("Key Indicator(s) Summary:").bold());
+            doc.addParagraph(headingPara);
 
-        //Add Date Time stamp in footer
-        let timestampPara = doc.Footer.createParagraph("Report generated on ").right();
-        timestampPara.addRun(new docx.TextRun(dateTimeGenerator.getDateTimeNow()));
+            let indicatorPara = new docx.Paragraph();
+            indicatorPara.addRun(new docx.TextRun(""));
+            doc.addParagraph(indicatorPara);
+            doc.addParagraph(blankPara);
 
-        // Used to export the file into a .docx file
-        console.log('Exporting Document...');
-        let packer = new docx.Packer();
-        try 
-        {
-            packer.toBuffer(doc).then((buffer) => {
-                fs.writeFileSync("Status Summary Report.docx", buffer);
-            });
-            console.log('Document Exported!');
-        }
-        catch (error) {
-            throw error;
-        }
-        
+            //Add Trend(s) TODO
+            headingPara = new docx.Paragraph().heading1().left();
+            headingPara.addRun(new docx.TextRun("Trend(s) Summary:").bold());
+            doc.addParagraph(headingPara);
+
+            let trendPara = new docx.Paragraph();
+            trendPara.addRun(new docx.TextRun(""));
+            doc.addParagraph(trendPara);
+            doc.addParagraph(blankPara);
+
+            //Add Date Time stamp in footer
+            let timestampPara = doc.Footer.createParagraph("Report generated on ").right();
+            timestampPara.addRun(new docx.TextRun(dateTimeGenerator.getDateTimeNow()));
+
+            // Used to export the file into a .docx file
+            console.log('Exporting Document...');
+            let packer = new docx.Packer();
+            try {
+                packer.toBuffer(doc).then((buffer) => {
+                    fs.writeFileSync("Status Summary Report.docx", buffer);
+                });
+                console.log('Document Exported!');
+            }
+            catch (error) {
+                throw error;
+            }
+
+        });
+
+
+
     }
 };
