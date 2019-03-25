7ea8f331df2077c49c426f5216877c76650d0407
nigelawj <nigelawj@gmail.com>
2019-03-25 20:25:25 +0800

    Updated weather.js to send API data to SRM whenever it calls via socket 5001

--- CrisisMap/Weather.js
+++ CrisisMap/Weather.js
@@ -1,3 +1,17 @@
+var io = require('./Apps/node_modules/socket.io').listen(5001);
+var fetch = require('isomorphic-fetch');
+
+io.sockets.on('connect', function(socket){
+  console.log("Connected to SRM on port 5001");
+
+  socket.on('apiRequest', function(){
+    fetch("https://api.data.gov.sg/v1/environment/2-hour-weather-forecast").then(response => {
+      return response.json();
+    }).then(data => {
+      socket.emit('apiRequestDone', data);
+    });
+  });
+});
 
 function getData(){
     //alert('ajax');
@@ -13,7 +27,6 @@ function getData(){
             receivedData = data;
         }
     });
-
     // console.log("returning from function");
     return receivedData;
 };
