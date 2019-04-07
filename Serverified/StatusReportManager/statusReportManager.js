9057d5f8e20480952382ac06b68ab254a89c10f3
tehtea <jingwei.chew@gmail.com>
2019-04-07 09:21:13 -0700

    fixed up some bugs... again

--- Serverified/StatusReportManager/statusReportManager.js
+++ Serverified/StatusReportManager/statusReportManager.js
@@ -9,11 +9,7 @@ var reportGenerated;
 module.exports = {
 	runStatusReport: async function() {
 		cron.schedule('*/30 * * * *', () => {  //EVERY 30 MINS (XX:00 & XX:30)
-<<<<<<< HEAD
-		// cron.schedule('*/60 * * * * *', () => {  //EVERY 1 MINUTE FOR TESTING
-=======
 		//cron.schedule('*/10 * * * * *', () => {  //EVERY 10 SECONDS FOR TESTING
->>>>>>> master
 			reportGenerated = reportGenerator.generateReport();
   			reportGenerated.then(() => {
 				reportTransmitter.sendEmail();
